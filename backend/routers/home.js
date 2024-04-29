3a254046f67ce5fc2bff0e88f87e849c80d77787
Arnaldo <113262554+ArnaldoLima12@users.noreply.github.com>
2024-04-28 22:41:59 -0300

    Delete backend directory

--- backend/routers/home.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express').Router();
-const Home = require('../controllers/HomeController');
-const auth = require('../middlewares/auth');
-
-Router.get('/', auth, Home.index)
-Router.get('/logout', Home.logout);
-
-module.exports = Router;
--- backend/routers/login.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router();
-const login = require('../controllers/LoginController.js');
-
-/**Rota de login*/
-Router.get('/', login.index);
-Router.post('/auth', login.auth);
-
-
-module.exports = Router;
